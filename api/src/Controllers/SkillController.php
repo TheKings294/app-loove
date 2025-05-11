8308b59a54c141197ad32ec9efef830dcb7c8a5a
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-05-11 20:02:20 +0200

    feat(Soft Skill): add soft skill feature at the projet

--- api/src/Controllers/SkillController.php
+++ api/src/Controllers/SkillController.php
@@ -1,1 +1,29 @@
 <?php
+
+namespace App\Controllers;
+
+use App\Repositories\SkillRepositories;
+use Monolog\Logger;
+use App\Models\SoftSkill;
+
+class SkillController extends BaseController
+{
+    private SkillRepositories $skillRepo;
+
+    public function __construct(Logger $logger)
+    {
+        parent::__construct($logger);
+        $this->skillRepo = new SkillRepositories();
+    }
+
+    public function setSkill(SoftSkill $skill): bool
+    {
+        $this->skillRepo->setSkill($skill);
+        return true;
+    }
+    public function updateSkill(SoftSkill $skill): bool
+    {
+        $this->skillRepo->updateSkill($skill);
+        return true;
+    }
+}
--- api/src/Repositories/SkillRepositories.php
+++ api/src/Repositories/SkillRepositories.php
@@ -1,1 +1,49 @@
 <?php
+
+namespace App\Repositories;
+
+use App\Models\SoftSkill;
+use App\Repositories\BaseRepositories;
+
+class SkillRepositories extends BaseRepositories
+{
+    public function setSkill(SoftSkill $skill): void
+    {
+        $this
+            ->query("INSERT INTO skills (user_id,skill1,skill2,skill3,skill4,skill5,skill6,skill7,skill8,skill9,skill10) 
+                            VALUES(:user_id, :skill1, :skill2, :skill3, :skill4, :skill5, :skill6, :skill7, :skill8, :skill9, :skill10)")
+            ->execute([
+                'user_id' => $skill->userId,
+                'skill1' => $skill->skill1,
+                'skill2' => $skill->skill2,
+                'skill3' => $skill->skill3,
+                'skill4' => $skill->skill4,
+                'skill5' => $skill->skill5,
+                'skill6' => $skill->skill6,
+                'skill7' => $skill->skill7,
+                'skill8' => $skill->skill8,
+                'skill9' => $skill->skill9,
+                'skill10' => $skill->skill10,
+            ]);
+    }
+    public function updateSkill(SoftSkill $skill): void
+    {
+        $this
+            ->query("UPDATE skills SET skill1 = :skill1,skill2 = :skill2,skill3 = :skill3,skill4 = :skill4,skill5 = :skill5,
+                            skill6 = :skill6,skill7 = :skill7,skill8 = :skill8,skill9 = :skill9,skill10 = :skill10
+                            WHERE user_id = :user_id;")
+            ->execute([
+                'user_id' => $skill->userId,
+                'skill1' => $skill->skill1,
+                'skill2' => $skill->skill2,
+                'skill3' => $skill->skill3,
+                'skill4' => $skill->skill4,
+                'skill5' => $skill->skill5,
+                'skill6' => $skill->skill6,
+                'skill7' => $skill->skill7,
+                'skill8' => $skill->skill8,
+                'skill9' => $skill->skill9,
+                'skill10' => $skill->skill10,
+            ]);
+    }
+}
