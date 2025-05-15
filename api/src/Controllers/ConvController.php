6833f7897a2ee4a98ea2cf81e70901669b005ed0
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-05-15 12:08:39 +0200

    feat(conv): add function to set an conv

--- api/src/Controllers/ConvController.php
+++ api/src/Controllers/ConvController.php
@@ -2,7 +2,20 @@
 
 namespace App\Controllers;
 
+use Monolog\Logger;
+use App\Repositories\ConvRepositories;
+
 class ConvController extends BaseController
 {
-
+    private ConvRepositories $convRepo;
+    public function __construct(Logger $logger)
+    {
+        parent::__construct($logger);
+        $this->convRepo = new ConvRepositories();
+    }
+    public function newConv(int $user_A, int $user_B): bool
+    {
+        $this->convRepo->newConv($user_A, $user_B);
+        return true;
+    }
 }
--- api/src/Repositories/ConvRepositories.php
+++ api/src/Repositories/ConvRepositories.php
@@ -4,5 +4,13 @@ namespace App\Repositories;
 
 class ConvRepositories extends BaseRepositories
 {
-    
+    public function newConv(int $user_A, int $user_B)
+    {
+        $this
+            ->query("INSERT INTO conversation (user_a, user_b) VALUES (:user_a, :user_b)")
+            ->execute([
+                'user_a' => $user_A,
+                'user_b' => $user_B
+            ]);
+    }
 }
