78533fcb01e471ca6dee96ceba71d6789fcd2fee
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-05-15 12:10:28 +0200

    feat(like): add code to set an match and an conv if is an macth

--- api/src/Controllers/LikeController.php
+++ api/src/Controllers/LikeController.php
@@ -1,1 +1,47 @@
 <?php
+
+namespace App\Controllers;
+
+use Monolog\Logger;
+use App\Repositories\LikeRepositories;
+use App\Utils\Functions;
+
+class LikeController extends BaseController
+{
+    private LikeRepositories $likeRepo;
+    public function __construct(Logger $logger)
+    {
+        parent::__construct($logger);
+        $this->likeRepo = new LikeRepositories();
+    }
+    public function LikeAnUser(string $userId, string $userLiked)
+    {
+        $userId = intval(Functions::cleanCodeString($userId));
+        $userLiked = intval(Functions::cleanCodeString($userLiked));
+
+        $this->likeRepo->setLike($userLiked, $userId);
+
+        if (!empty($this->likeRepo->checkIfIsMatch($userId, $userLiked))) {
+            $match = new MatchController($this->logger);
+            $conv = new ConvController($this->logger);
+            $match->newMatch($userId, $userLiked);
+            $conv->newConv($userId, $userLiked);
+
+            http_response_code(200);
+            return json_encode(['message' => 'Is match']);
+        }
+
+        http_response_code(200);
+        return json_encode(['message' => 'Liked is save']);
+    }
+    public function setUnlike(string $userId, string $userLiked)
+    {
+        $userId = intval(Functions::cleanCodeString($userId));
+        $userLiked = intval(Functions::cleanCodeString($userLiked));
+
+        $this->likeRepo->setUnlike($userLiked, $userId);
+
+        http_response_code(200);
+        return json_encode(['message' => 'Unliked is save']);
+    }
+}
--- api/src/Repositories/LikeRepositories.php
+++ api/src/Repositories/LikeRepositories.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace App\Repositories;
+
+use App\Repositories\BaseRepositories;
+
+class LikeRepositories extends BaseRepositories
+{
+    public function setLike(int $userLiked, int $userId)
+    {
+        $this
+            ->query("INSERT INTO like (user_liked, user) VALUES (:userLiked, :user)")
+            ->execute([
+                'userLiked' => $userLiked,
+                'user' => $userId
+            ]);
+    }
+    public function checkIfIsMatch(int $userId, int $userLiked)
+    {
+        $result = $this
+            ->query("SELECT * FROM like WHERE user_liked = :userLiked AND user = :user")
+            ->execute([
+                'userLiked' => $userId,
+                'user' => $userLiked
+            ]);
+
+        return $result;
+    }
+    public function setUnlike(int $userUnliked, int $userId)
+    {
+        $this
+            ->query("INSERT INTO un_like (user_unlike, user) VALUES (:userLiked, :user)")
+            ->execute([
+                'userLiked' => $userUnliked,
+                'user' => $userId
+            ]);
+    }
+}
