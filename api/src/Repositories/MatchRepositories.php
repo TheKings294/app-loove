159be1b902fc47b71c53b60c3f82281405871e29
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-05-15 12:09:12 +0200

    feat(match): add function to set an match

--- api/src/Repositories/MatchRepositories.php
+++ api/src/Repositories/MatchRepositories.php
@@ -4,5 +4,13 @@ namespace App\Repositories;
 
 class MatchRepositories extends BaseRepositories
 {
-
+    public function newMatch(int $user_A, int $user_B): void
+    {
+        $this
+            ->query("INSERT INTO matches (user_a, user_b) VALUES (:user_A, :user_B)")
+            ->execute([
+                "user_A" => $user_A,
+                "user_B" => $user_B
+            ]);
+    }
 }
